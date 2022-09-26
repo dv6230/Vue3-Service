34d200952a99e919bc50a997ee5fa48cc275edea
Allis <dveasia558@gmail.com>
2022-09-26 23:58:10 +0800

    加入專案檔案。

---
+++ Controllers/TestController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Vue3_Service.Controllers
+{
+    [Route("api/test/[action]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult Test1()
+        {
+            return Ok(100);
+        }
+    }
+}
