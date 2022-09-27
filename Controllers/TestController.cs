4e690d031a3e4d6370d8aa2f948c6df0cb21f4b1
allisonhung <allisonhung@iscom.com.tw>
2022-09-27 11:16:42 +0800

    Filter 製作

--- Controllers/TestController.cs
+++ Controllers/TestController.cs
@@ -1,15 +1,25 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Vue3_Service.Filters;
 
 namespace Vue3_Service.Controllers
 {
-    [Route("api/test/[action]")]
-    [ApiController]
-    public class TestController : ControllerBase
-    {
-        [HttpPost]
-        public IActionResult Test1()
-        {
-            return Ok(100);
-        }
-    }
+	[Route("api/test/[action]")]
+	[ApiController]
+	public class TestController : ControllerBase
+	{
+		[HttpPost]
+
+		public IActionResult Test1()
+		{
+			return Ok(100);
+		}
+
+		[HttpGet]
+		[ActionFilter(str:"test")]
+		public IActionResult Test2()
+		{
+			return Ok(200);
+		}
+	}
 }
---
+++ Filters/AuthorizationFilter.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.Primitives;
+
+namespace Vue3_Service.Filters
+{
+	public class ActionFilter : Attribute, IActionFilter
+	{
+		string str = "";
+		public ActionFilter(string str)
+		{
+			this.str = str;
+		}
+
+		public void OnActionExecuting(ActionExecutingContext context)
+		{
+			StringValues auth;
+			context.HttpContext.Request.Headers.TryGetValue("auth", out auth);
+			if (auth.ToString() != "test")
+			{
+				context.Result = new OkObjectResult(new { status = "fail", message = "權限不足" });
+			}
+			else
+			{
+				throw new InvalidOperationException("Request is not valid!");
+			}
+		}
+
+		public void OnActionExecuted(ActionExecutedContext context)
+		{
+			//context.HttpContext.Response.WriteAsync($"{GetType().Name} out. \r\n");
+		}
+	}
+}
