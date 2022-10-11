1c9fa737fe901518320dfeb82b87b48332ff7ac4
allisonhung <allisonhung@iscom.com.tw>
2022-10-11 10:06:47 +0800

    加入 entity framework 模型

---
+++ Entity/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Vue3_Service.Entity
+{
+    public partial class User
+    {
+        public User()
+        {
+            UserLogins = new HashSet<UserLogin>();
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public string Password { get; set; } = null!;
+        public DateTime? CreateTime { get; set; }
+
+        public virtual ICollection<UserLogin> UserLogins { get; set; }
+    }
+}
---
+++ Entity/UserLogin.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Vue3_Service.Entity
+{
+    public partial class UserLogin
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public string? Token { get; set; }
+        public DateTime? ExpireTime { get; set; }
+
+        public virtual User User { get; set; } = null!;
+    }
+}
