b6da8f33ff038b7ed7cd9e585127ca4005210b44
allisonhung <allisonhung@iscom.com.tw>
2022-10-11 10:07:14 +0800

    登入 API

---
+++ Controllers/UserController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Vue3_Service.Parameter;
+
+namespace Vue3_Service.Controllers
+{
+	[Route("api/user/[action]")]
+	[ApiController]
+	public class UserController : ControllerBase
+	{
+		public IActionResult Login([FromBody] Login data)
+		{
+
+			return Ok(100);
+		}
+	}
+}
